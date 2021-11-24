9c437bb4c3dbecb5f46b92a7d1f8883fa2f33533
Jan Singon Slany <singond@seznam.cz>
2021-11-24 23:05:54 +0100

    01: Group data belonging to single experiments in calculations

--- 01_network_analyzer/export.m
+++ 01_network_analyzer/export.m
@@ -1,18 +1,38 @@
 pkg load report
 main
 
-for e = experiment
+function plot_sparams(freq, s, name, elem)
 	gp = gnuplotter();
 	p1 = gp.newplot();
-	p1.plot(e.freq, real(e.s), "with lines ls 2");
+	p1.plot(freq, real(s), "with lines ls 2");
 	p2 = gp.newplot();
-	p2.plot(e.freq, imag(e.s), "with lines ls 1");
+	p2.plot(freq, imag(s), "with lines ls 1");
 	gp.exec("set term epslatex size 16cm,10cm");
-	gp.exec(sprintf("set output 'plots/%s.tex'", e.name));
+	gp.exec(sprintf("set output 'plots/%s.tex'", [name "-" elem]));
 	gp.multiplot(2,1);
 	gp.load("real.gp");
+	if (strcmp(elem, "s11"))
+		gp.exec("set ylabel '$\\Re(\\sparam_{11})\\,[\\si{\\decibel}]$'");
+	elseif (strcmp(elem, "s21"))
+		gp.exec("set ylabel '$\\Re(\\sparam_{21})\\,[\\si{\\decibel}]$'");
+	endif
 	gp.doplot(p1);
 	gp.load("imag.gp");
+	if (strcmp(elem, "s11"))
+		gp.exec("set ylabel '$\\Im(\\sparam_{11})\\,[\\si{\\degree}]$'");
+	elseif (strcmp(elem, "s21"))
+		gp.exec("set ylabel '$\\Im(\\sparam_{21})\\,[\\si{\\degree}]$'");
+	end
 	gp.doplot(p2);
 	clear gp;
+endfunction
+
+for e = experiment
+	printf("Plotting experiment %s.\n", e.name);
+	if (!isempty(e.s11))
+		plot_sparams(e.freq, e.s11, e.name, "s11");
+	end
+	if (!isempty(e.s21))
+		plot_sparams(e.freq, e.s21, e.name, "s21");
+	end
 endfor
--- 01_network_analyzer/main.m
+++ 01_network_analyzer/main.m
@@ -1,14 +1,66 @@
+0; # Not a function file
+
+## Import scattering matrix from data files.
+function ex = import_smatrix(s11_filename, s21_filename, name)
+	if (nargin < 3)
+		[~, fname, ~] = fileparts(s11_filename);
+		name = strsplit(fname, "-"){1};
+	end
+	ex = struct("name", name, "freq", [], "s11", [], "s21", []);
+
+	# Read s_11 data
+	if (!isempty(s11_filename))
+		printf("Reading s_11 data from %s.\n", s11_filename);
+		f = fopen(["data/" s11_filename]);
+		fskipl(f, 2);
+		header = fgetl(f);
+		assert(strcmp(strtrim(header), "freq[Hz] db:Trc1_S11 ang:Trc1_S11"),
+			"File %s does not contain s_11 data. The header is: %s.",
+			s11_filename, header);
+		data = dlmread(f, "");
+		fclose(f);
+		ex.freq = data(:,1);
+		ex.s11 = complex(data(:,2), data(:,3));
+	end
+
+	# Read s_21 data
+	if (!isempty(s21_filename))
+		printf("Reading s_21 data from %s.\n", s21_filename);
+		f = fopen(["data/" s21_filename]);
+		fskipl(f, 2);
+		header = fgetl(f);
+		assert(strcmp(strtrim(header), "freq[Hz] db:Trc1_S21 ang:Trc1_S21"),
+			"File %s does not contain s_21 data.", s21_filename);
+		data = dlmread(f, "");
+		fclose(f);
+		if (!isempty(ex.freq))
+			assert(all(data(:,1) == ex.freq),
+				"Frequency data differs between %s and %s.",
+				s11_filename, s21_filename);
+		else
+			ex.freq = data(:,1);
+		endif
+		ex.s21 = complex(data(:,2), data(:,3));
+	end
+end
+
 ## Import the data into an array of structs called 'experiment'.
-filenames = glob("data/data*.csv");
-experiment = struct;
-j = 1;
-for file = filenames'
-    file = file{1};
-    idx = j++;
-    data = dlmread(file, "", 3, 0);
-    [~, name, ext] = fileparts(file);
-    experiment(idx).name = name;
-    experiment(idx).filename = [name ext];
-    experiment(idx).freq = data(:,1);
-    experiment(idx).s = complex(data(:,2), data(:,3));
-endfor
+ex = struct("name", {}, "freq", {}, "s11", {}, "s21", {});
+k = 1;
+experiment(k++) = import_smatrix("data01-s11.csv", "data01-s21.csv");
+experiment(k++) = import_smatrix("data02-s11.csv", "data02-s21.csv");
+experiment(k++) = import_smatrix("data03-s11.csv", "data03-s21.csv");
+experiment(k++) = import_smatrix("data04-s11.csv", "data04-s21.csv");
+experiment(k++) = import_smatrix("data05-s11.csv", "data05-s21.csv");
+experiment(k++) = import_smatrix("data06-s11.csv", "data06-s21.csv");
+experiment(k++) = import_smatrix("data07-s11.csv", "data07-s21.csv");
+experiment(k++) = import_smatrix("data08-s11.csv", "data08-s21.csv");
+experiment(k++) = import_smatrix("data09-s11.csv", "data09-s21.csv");
+experiment(k++) = import_smatrix("data10-s11.csv", "data10-s21.csv");
+experiment(k++) = import_smatrix("data11-s11.csv", "");
+experiment(k++) = import_smatrix("data12-s11.csv", "");
+experiment(k++) = import_smatrix("data13-s11.csv", "");
+experiment(k++) = import_smatrix("data14-s11.csv", "");
+experiment(k++) = import_smatrix("data15-s11.csv", "");
+experiment(k++) = import_smatrix("data16-s11.csv", "");
+experiment(k++) = import_smatrix("", "data17-s21.csv", "data17");
