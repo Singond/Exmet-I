9ab7e8d6c0ead33ca44f68dd1696c34bf6982acb
Jan Singon Slany <singond@seznam.cz>
2021-11-26 00:26:46 +0100

    01: Fix handling complex numbers

--- 01_network_analyzer/export.m
+++ 01_network_analyzer/export.m
@@ -4,24 +4,24 @@ main
 function plot_sparam(freq, s, name, elem)
 	gp = gnuplotter();
 	p1 = gp.newplot();
-	p1.plot(freq, real(s), "with lines ls 2");
+	p1.plot(freq, mag2db(abs(s)), "with lines ls 2");
 	p2 = gp.newplot();
-	p2.plot(freq, imag(s), "with lines ls 1");
+	p2.plot(freq, rad2deg(arg(s)), "with lines ls 1");
 	gp.exec("set term epslatex size 16cm,10cm");
 	gp.exec(sprintf("set output 'plots/%s.tex'", [name "-" elem]));
 	gp.multiplot(2,1);
 	gp.load("real.gp");
 	if (strcmp(elem, "s11"))
-		gp.exec("set ylabel '$\\Re(\\sparam_{11})\\,[\\si{\\decibel}]$'");
+		gp.exec("set ylabel '$|\\sparam_{11}|\\,[\\si{\\decibel}]$'");
 	elseif (strcmp(elem, "s21"))
-		gp.exec("set ylabel '$\\Re(\\sparam_{21})\\,[\\si{\\decibel}]$'");
+		gp.exec("set ylabel '$|\\sparam_{21}|\\,[\\si{\\decibel}]$'");
 	endif
 	gp.doplot(p1);
 	gp.load("imag.gp");
 	if (strcmp(elem, "s11"))
-		gp.exec("set ylabel '$\\Im(\\sparam_{11})\\,[\\si{\\degree}]$'");
+		gp.exec("set ylabel '$\\arg{\\sparam_{11}}\\,[\\si{\\degree}]$'");
 	elseif (strcmp(elem, "s21"))
-		gp.exec("set ylabel '$\\Im(\\sparam_{21})\\,[\\si{\\degree}]$'");
+		gp.exec("set ylabel '$\\arg{\\sparam_{21}}\\,[\\si{\\degree}]$'");
 	end
 	gp.doplot(p2);
 	clear gp;
