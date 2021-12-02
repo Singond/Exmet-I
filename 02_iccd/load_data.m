34fe46e952026203a8fd14f104dfe2df2afb93db
Jan Singon Slany <singond@seznam.cz>
2021-12-02 21:29:34 +0100

    02: Correct offset image data

--- 02_iccd/load_data.m
+++ 02_iccd/load_data.m
@@ -1,3 +1,12 @@
 addpath octave
 
 [img hdr] = readSPE("data/hydrogen-ccp.spe");
+
+## In subsequent frames, the pixel columns are shifted
+## by an increasing amount. The following corrects this offset.
+for fn = 1:size(img, 3)
+	offset = 2 * (fn - 1);  # Amount of offset
+	frame = img(:,:,fn);
+	img(:,:,fn) = frame(:,[offset+1:end 1:offset]);
+endfor
+clear fn frame offset
